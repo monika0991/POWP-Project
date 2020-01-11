3ae6120b3358bfb63536f2beebc758848f7c5dc5
monika0991 <33000965+monika0991@users.noreply.github.com>
2020-01-11 21:13:02 +0100

    Lazy loading design pattern added.

--- src/locations/Floor.java
+++ src/locations/Floor.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Floor extends Location {
     private int number;
     private List<Room> rooms;
+    private String opis;
+    private boolean floorHasChanged = true;
 
     public Floor(int number) {
         this.number = number;
@@ -22,20 +24,32 @@ public class Floor extends Location {
     public void addRoom(Room room) {
         room.setFloor(locationID);
         rooms.add(room);
+        floorHasChanged = true;
     }
 
     public int getNumberOfRooms() {
         return rooms.size();
     }
 
+    // Lazy loading - pietro moze zostac stworzone, ale opis zostanie wygenerowany dopiero wtedy,
+    // gdy ktos wywola metode opis()
+    // dzieki temu bedziemy iterowac po pokojach tylko wtedy, gdy bedzie to konieczne
     @Override
     public String opis() {
+        if (floorHasChanged) {
+            generateOpis();
+        }
+        return opis;
+    }
+
+    private void generateOpis() {
         StringBuilder sB = new StringBuilder();
         sB.append(" Floor number: " + number + "\t[" + locationID + "]\n");
         for (Room room : rooms) {
             sB.append(room.opis());
         }
-        return sB.toString();
+        this.opis = sB.toString();
+        this.floorHasChanged = false;
     }
 
 }
